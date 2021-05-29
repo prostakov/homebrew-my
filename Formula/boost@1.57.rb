dd5ceb0ce41958cdc941784f0673180a79096fca
Serhii Prostakov <serhii.prostakov@gmail.com>
2021-05-29 21:54:35 +0300

    Update

--- Formula/boost@1.57.rb
+++ Formula/boost@1.57.rb
@@ -16,7 +16,7 @@ class BoostAT157 < Formula
   
     keg_only :versioned_formula
   
-    disable! date: "2020-09-01", because: :versioned_formula
+    #disable! date: "2020-09-01", because: :versioned_formula
   
     # Fix build on Xcode 11.4
     patch do
